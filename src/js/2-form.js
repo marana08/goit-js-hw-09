b957773e4e98deafe8fecea9dfea4d38e8f93ee3
mariana <maranamelen13@gmail.com>
2025-11-09 18:21:24 +0200

    galleryandform

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,46 @@
-console.log("Fotm");
+const formData = {
+    email: '',
+    message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    try {
+        const parsedData = JSON.parse(savedData);
+        formData.email = parsedData.email || '';
+        formData.message = parsedData.message || '';
+
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    } catch (error) {
+        console.error('Помилка читання даних із localStorage:', error);
+    }
+}
+
+form.addEventListener('input', event => {
+    const { name, value } = event.target;
+
+    formData[name] = value.trimStart();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const { email, message } = formData;
+
+    if (email.trim() === '' || message.trim() === '') {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData.email = '';
+    formData.message = '';
+});
